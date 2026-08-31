d02e8db0ad009eeff5afd4c28e7ba888dbea42d6
zebben <zachebben@gmail.com>
2026-08-31 11:36:11 -0500

    fix

--- include/bag.h
+++ include/bag.h
@@ -268,7 +268,7 @@ void *LONG_CALL BagView_New(u8 heap_id);
 void LONG_CALL BagView_SetItem(void *bagView, ITEM_SLOT *slots, u8 pocketId, u8 position);
 // param1 is likely BagController (BAG_VIEW?)
 String *LONG_CALL BagApp_SetFlute(void *param1, int param2);
-String *LONG_CALL BagApp_TryUseRepel(int param1, u16 itemId);
+String *LONG_CALL BagApp_TryUseRepel(void *param1, u16 itemId);
 String *LONG_CALL BagApp_ToggleGBSounds(void *param1, int itemId);
 
 #endif // POKEHEARTGOLD_BAG_H
